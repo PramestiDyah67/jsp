17b9514c3792f459f837223d121de7b29dab3a3d
PramestiDyah67 <62537342+PramestiDyah67@users.noreply.github.com>
2020-05-18 16:27:59 +0700

    Revisi Model
    Revisi

--- BiodataModel.java
+++ BiodataModel.java
@@ -1,62 +1,49 @@
 package com.model.mahasiswa;
 
-import java.beans.Statement; 
-import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
+
 public class BiodataModel {
-	Connection con;
-	Statement st;
-	ResultSet rs;
-	Biodata biodata;
-	ArrayList<Biodata> biodatas = new ArrayList<>();
+Connection con;
+Statement st;
+ResultSet rs;
+Biodata biodata;
+ArrayList<Biodata> biodatas=new ArrayList<>();
+
+public BiodataModel(){
 	
-	public BiodataModel() {
-		try {
-			/*Class.forName("com.mysql.jdbc.Driver");
-			con=(Connection)
-			DriverManager.getConnection("jdbc:mysql://localhost:3306/mhs");
-			*/
-			String url ="jdbc:mysql://localhost:3306/mhs?user=root&password=\"";
-            String user="root";
-            String pass="";
-            Class.forName("com.mysql.jdbc.Driver");
-            con =(Connection) DriverManager.getConnection(url,user,pass);//koneksi ke database
-            st = (Statement) con.createStatement();//statement untuk eksekusi query
-            System.out.println("koneksi berhasil;");
-				Class.forName("com.mysql.jdbc.Driver");
-				 con =(Connection) DriverManager.getConnection(url,user,pass);
-				 st = (Statement) con.createStatement();
-				 System.out.println("koneksi berhasil;");
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			System.err.println("koneksi gagal" +e.getMessage());
-		}catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
+	try {
+	Class.forName("com.mysql.jdbc.Driver");
+	con=(Connection)
+	DriverManager.getConnection("jdbc:mysql://localhost:3306/mhs?user=root&password=");
+	} catch (ClassNotFoundException | SQLException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
 	}
+} 
 	public ArrayList<Biodata> getData(){
-		try {
-			st=(Statement) con.createStatement();
-			rs=((java.sql.Statement) st).executeQuery("SELECT * FROM biodata");
-			while(rs.next()) {
-				biodata = new Biodata();
-				biodata.setNim(rs.getString(1));
-				biodata.setNama(rs.getString(2));
-				biodata.setJk(rs.getString(3));
-				biodata.setTmpLahir(rs.getString(4));
-				biodata.setTglLahir(rs.getString(5));
-				biodata.setAlamat(rs.getString(6));
-				biodatas.add(biodata);
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return biodatas;	
+	try {
+	st=(Statement) con.createStatement();
+	rs=st.executeQuery("SELECT * FROM biodata");
+	while(rs.next()){
+	biodata=new Biodata();
+	biodata.setNim(rs.getString(1));
+	biodata.setNama(rs.getString(2));
+	biodata.setJk(rs.getString(3));
+	biodata.setTmpLahir(rs.getString(4));
+	biodata.setTglLahir(rs.getString(5));
+	biodata.setAlamat(rs.getString(6));
+	biodatas.add(biodata);
 	}
+} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+return biodatas;
+}
 }
